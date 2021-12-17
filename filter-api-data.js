a55f7f741202f3e0a52161c3b5eecb5fb2ba5aa7
patricia prager <florence.voltaire@web.de>
2021-12-17 11:25:23 +0100

    kata solving

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,22 @@
 function filterApiData(apiData, mandatoryKeys) {
   // Add you solution here
+  // Datentyp filtern
+
+  let filterArr = apiData.filter(function (itemsKey) {
+    let arr = Object.keys(itemsKey);
+    let check = true;
+    for (let i = 0; i < mandatoryKeys.length; i++) {
+      if (!arr.includes(mandatoryKeys[i])) {
+        check = false;
+      }
+      console.log(mandatoryKeys[i]);
+    }
+
+    if (check === true) {
+      return true;
+    }
+    // console.log(mandatoryKeys[i]);
+    console.log(arr);
+  });
+  return filterArr;
 }
